10f3fff8b1cb3cdedec0b91e520cb549f32df90a
giovannamoeller <giovannagmoeller@gmail.com>
2021-06-08 18:10:38 -0300

    Implementing modules for getting date

---
+++ js/components/Date.js
@@ -0,0 +1,37 @@
+const getDate = () => {
+    const date = new Date();
+    let weekDay, month;
+    const day = date.getDate();
+
+    switch(date.getDay()) {
+        case 1: weekDay = 'Monday'; break;
+        case 2: weekDay = 'Tuesday'; break;
+        case 3: weekDay = 'Wednesday'; break;
+        case 4: weekDay = 'Thursday'; break;
+        case 5: weekDay = 'Friday'; break;
+        case 6: weekDay = 'Saturday'; break;
+        case 7: weekDay = 'Sunday'; break;
+    }
+
+    switch(date.getMonth() + 1) {
+        case 1: month = 'jan'; break;
+        case 2: month = 'fev'; break;
+        case 3: month = 'mar'; break;
+        case 4: month = 'apr'; break;
+        case 5: month = 'may'; break;
+        case 6: month = 'jun'; break;
+        case 7: month = 'jul'; break;
+        case 8: month = 'aug'; break;
+        case 9: month = 'sep'; break;
+        case 10: month = 'oct'; break;
+        case 11: month = 'nov'; break;
+        case 12: month = 'dec'; break;
+    }
+
+    const fullDate = `${weekDay}, ${day} ${month}.`;
+
+    return fullDate;
+
+}
+
+export default getDate;
